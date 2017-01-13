f6c29e81f1e4bb56f58db78a593d3fd35677d3be
duncan.devore@gmail.com <duncan.devore@gmail.com>
2017-01-12 20:34:16 -0500

    reformated exercise 011

--- exercise_011_Faulty_guest/src/test/java/com/lightbend/training/coffeehouse/WaiterTest.java
+++ exercise_011_Faulty_guest/src/test/java/com/lightbend/training/coffeehouse/WaiterTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 
 public class WaiterTest extends BaseAkkaTestCase {
 
-  @Test
-  public void sendingServeCoffeeShouldResultInApproveCoffeeToCoffeeHouse() {
-    new JavaTestKit(system) {{
-      ActorRef coffeeHouse = getRef();
-      TestProbe guest = new TestProbe(system);
-      ActorRef waiter = system.actorOf(Waiter.props(coffeeHouse));
-      waiter.tell(new Waiter.ServeCoffee(new Coffee.Akkaccino()), guest.ref());
-      expectMsgEquals(new CoffeeHouse.ApproveCoffee(new Coffee.Akkaccino(), guest.ref()));
-    }};
-  }
+    @Test
+    public void sendingServeCoffeeShouldResultInApproveCoffeeToCoffeeHouse() {
+        new JavaTestKit(system) {{
+            ActorRef coffeeHouse = getRef();
+            TestProbe guest = new TestProbe(system);
+            ActorRef waiter = system.actorOf(Waiter.props(coffeeHouse));
+            waiter.tell(new Waiter.ServeCoffee(new Coffee.Akkaccino()), guest.ref());
+            expectMsgEquals(new CoffeeHouse.ApproveCoffee(new Coffee.Akkaccino(), guest.ref()));
+        }};
+    }
 }
